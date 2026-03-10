00e8ebeee4aaab8c9831576594887459999e6a44
Neslihan Cesur <137646798+neslihancesurr@users.noreply.github.com>
2026-03-10 16:41:33 +0300

    fix find_packages() in setup.py
    Due to a hard-coded line on the setup.py file, pip did not install all the folders. Added find_packages() instead to fix the pip installation issue

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
-
 setup(
     name='NlpToolkit-ComputationalGraph',
     version='1.0.0',
-    packages=['ComputationalGraph'],
+    packages=find_packages(),
     url='https://github.com/StarlangSoftware/ComputationalGraph-Py',
     license='',
     author='olcaytaner',
